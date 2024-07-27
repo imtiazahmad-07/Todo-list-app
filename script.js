1d6a69a51d358e13194d69bc570ef1179ba03e0a
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-07-27 21:51:10 +0500

    Writing the logic

---
+++ script.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("#btn")
+let inputs = document.querySelector("#text-input")
+let text = document.querySelector(".text")
+
+btn.addEventListener('click', ()=>{
+    if(inputs.value == "")
+        {
+            alert("Enter the task please!")
+        }
+        else
+        {
+            let newElement = document.createElement("p");
+            newElement.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
+            text.appendChild(newElement);
+            inputs.value= "";
+
+        }
+})
+
